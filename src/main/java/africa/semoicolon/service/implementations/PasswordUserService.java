860b562fda280feaca2e6959c1e301b05cbfbf22
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-14 20:41:37 +0100

    commit

---
+++ src/main/java/africa/semoicolon/service/implementations/PasswordUserService.java
@@ -0,0 +1,9 @@
+package africa.semoicolon.service.implementations;
+
+import africa.semoicolon.service.interFace.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordUserService implements UserService{
+
+}
--- src/main/java/africa/semoicolon/service/interFace/UserService.java
+++ src/main/java/africa/semoicolon/service/interFace/UserService.java
@@ -1,5 +1,7 @@
 package africa.semoicolon.service.interFace;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public interface UserService{
 }
